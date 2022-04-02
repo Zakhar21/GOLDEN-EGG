0b88fbf62993ef9216a5779158c2a2c24d46b41a
Zakhar21 <ip609108@gmail.com>
2022-04-02 18:12:41 +0700

    Добавил функцию для адаптивных табов

--- js/app.js
+++ js/app.js
@@ -8,3 +8,18 @@ const swiper = new Swiper('.swiper', {
     },
   
   });
+
+window.addEventListener('load', overflowInnerTabs);
+window.addEventListener('resize', overflowInnerTabs);
+
+function overflowInnerTabs(){
+  let outerTabs = document.querySelector('.tabs__container');
+  let innerTabs = document.querySelector('.tabs');
+
+  if(innerTabs.offsetWidth > outerTabs.offsetWidth){
+    innerTabs.classList.add('overflow');
+  }else{
+    innerTabs.classList.remove('overflow');
+  }
+}
+

c536961295cc488d975e4ff821a59ad7b4f7f6db
Zakhar21 <ip609108@gmail.com>
2022-04-03 13:27:17 +0700

    Сделал слайдер в табах
    Использовал Swiper 8 версии.
    Вместо slidesPerColumn - grid {
    rows:2,
    } и swiper-wrapper: flex-wrap:wrap

--- js/app.js
+++ js/app.js
@@ -1,13 +1,49 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.spec__swiper', {
     // Optional parameters
     loop: true,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable:true,
     },
   
-  });
+});
+const swiperMenu = new Swiper('.menu__slider', {
+  // Optional parameters
+  slidesPerView:2,
+  grid: {
+    rows: 2,
+  },
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+
+});
+
+
+
+let tabs = document.querySelector('.tabs');
+let menuSlider = document.querySelectorAll('.menu__slider-block');
+
+tabs.addEventListener('click', function (e) {
+  if(e.target.classList.contains('tabs__item')) {
+    this.querySelectorAll('.tabs__item').forEach(item => item.classList.remove('active'));
+    e.target.classList.add('active');
+  }
+
+  menuSlider.forEach(slider => slider.classList.remove('show'));
+
+  let tabIndex = e.target.dataset.tab;
+  let thisSwiper = document.querySelector(tabIndex);
+  thisSwiper.classList.add('show');
+});
+
+
 
 window.addEventListener('load', overflowInnerTabs);
 window.addEventListener('resize', overflowInnerTabs);
